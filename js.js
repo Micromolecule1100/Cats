ecb17a5047f560953670fcd135ff06f509cff284
George Lobodenko <loodenkog@gmail.com>
2018-08-08 18:00:24 +0300

    Cats

---
+++ js.js
@@ -0,0 +1,29 @@
+
+function f() {
+    var Images = document.getElementsByTagName('img');
+    for(var i = 0; i < Images.length; i++){
+        if (Images[i].hidden == false) Images[i].hidden = true;
+    }
+    return;
+}
+
+function click1() {
+    f();
+    var p = document.getElementById('1').hidden = false;}
+function click2() {
+    f();
+    var p = document.getElementById('2').hidden = false;}
+function click3() {
+    f();
+    var p = document.getElementById('3').hidden = false;}
+function click4() {
+    f();
+    var p = document.getElementById('4').hidden = false;}
+function click5() {
+    f();
+    var p = document.getElementById('5').hidden = false;}
+function click6() {
+    f();
+    var p = document.getElementById('6').hidden = false;}
+
+
